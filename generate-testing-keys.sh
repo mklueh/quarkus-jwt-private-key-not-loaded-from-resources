87eee70d64cf364e5a1065f6f864c16d541dda00
mklueh90 <marianklueh@gmail.com>
2020-11-01 17:09:08 +0100

    generate-testing-keys.sh replaced with functional script

--- generate-testing-keys.sh
+++ generate-testing-keys.sh
@@ -1,4 +1,9 @@
-ssh-keygen -t rsa -P "" -b 4096 -m PEM -f src/main/resources/META-INF/resources/privateKey.pem
-ssh-keygen -e -m PEM -f jwtRS256.key > src/main/resources/META-INF/resources/publicKey.pem
-cp src/main/resources/META-INF/resources/privateKey.pem src/test/resources/META-INF/resources/privateKey.pem
+mkdir -p src/main/resources/META-INF/resources
+
+ssh-keygen -t rsa -P "" -b 4096 -m PEM -f src/main/resources/privateKey.pem
+ssh-keygen -e -m PEM -f src/main/resources/privateKey.pem > src/main/resources/META-INF/resources/publicKey.pem
+
+mkdir -p src/test/resources/META-INF/resources
+
+cp src/main/resources/privateKey.pem src/test/resources/privateKey.pem
 cp src/main/resources/META-INF/resources/publicKey.pem src/test/resources/META-INF/resources/publicKey.pem
